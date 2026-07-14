7bcf2bb4633648409aa6f539091f86618f61f1c2
dudruz <125500692+dudruz@users.noreply.github.com>
2026-07-14 14:37:31 -0300

    Update env.js

--- js/env.js
+++ js/env.js
@@ -7,6 +7,6 @@
  */
 window.DuAmigoEnv = Object.freeze({
     SUPABASE_URL: "https://zglooskfheyjlcbpgbwh.supabase.co",
-    SUPABASE_ANON_KEY: "",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnbG9vc2tmaGV5amxjYnBnYndoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwMzE4MDgsImV4cCI6MjA5OTYwNzgwOH0.sI8UgyKgWEpopLa6wjcobJQhG1f5xuw7hEyO9SopI5Q",
     STORAGE_BUCKET: "product-images"
 });
