08c6132a601b0bb6fa34dbfc63cc268681ede935
dudruz <125500692+dudruz@users.noreply.github.com>
2026-07-14 12:10:29 -0300

    Update env.js

--- js/env.js
+++ js/env.js
@@ -6,7 +6,7 @@
  * Nunca use a service_role key neste arquivo.
  */
 window.DuAmigoEnv = Object.freeze({
-    SUPABASE_URL: "",
-    SUPABASE_ANON_KEY: "",
+    SUPABASE_URL: "https://zglooskfheyjlcbpgbwh.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnbG9vc2tmaGV5amxjYnBnYndoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwMzE4MDgsImV4cCI6MjA5OTYwNzgwOH0.sI8UgyKgWEpopLa6wjcobJQhG1f5xuw7hEyO9SopI5Q",
     STORAGE_BUCKET: "product-images"
 });
